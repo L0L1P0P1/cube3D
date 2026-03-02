62b1ca7414dd5344d6de61349fd25e39519aa7f1
L0L1P0P1 <behradbadeli@gmail.com>
2026-03-03 01:04:41 +0330

    better object

--- cube.cpp
+++ cube.cpp
@@ -1,6 +1,4 @@
-#include <chrono>
 #include <cmath>
-#include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <thread>
@@ -21,10 +19,6 @@ public:
 
   Point rotate_x(double alpha)
   {
-    // double& x = this->x;
-    // double& y = this->y;
-    // double& z = this->z;
-
     return Point(x,
                  y * std::cos(alpha) - z * std::sin(alpha),
                  y * std::sin(alpha) + z * std::cos(alpha));
@@ -32,10 +26,6 @@ public:
 
   Point rotate_y(double beta)
   {
-    // double& x = this->x;
-    // double& y = this->y;
-    // double& z = this->z;
-    //
     return Point(x * std::cos(beta) + z * std::sin(beta),
                  y,
                  -x * std::sin(beta) + z * std::cos(beta));
@@ -43,17 +33,13 @@ public:
 
   Point rotate_z(double gamma)
   {
-    // double& x = this->x;
-    // double& y = this->y;
-    // double& z = this->z;
-
     return Point(x * std::cos(gamma) - y * std::sin(gamma),
                  x * std::sin(gamma) + y * std::cos(gamma),
                  z);
   }
 
-  Point operator+(Point& rhs) { return Point(x + rhs.x, y + rhs.y, z + rhs.z); }
-  Point operator-(Point& rhs) { return Point(x - rhs.x, y - rhs.y, z - rhs.z); }
+  Point operator+(Point rhs) { return Point(x + rhs.x, y + rhs.y, z + rhs.z); }
+  Point operator-(Point rhs) { return Point(x - rhs.x, y - rhs.y, z - rhs.z); }
 };
 
 struct Orientation
@@ -68,53 +54,60 @@ struct Orientation
     , gamma(gamma)
   {
   }
+
+  Orientation operator+(Orientation rhs)
+  {
+    return Orientation(alpha + rhs.alpha, beta + rhs.beta, gamma + rhs.gamma);
+  }
+  Orientation operator-(Orientation rhs)
+  {
+    return Orientation(alpha - rhs.alpha, beta - rhs.beta, gamma - rhs.gamma);
+  }
+
+  Orientation operator+=(Orientation rhs) { return *this + rhs; }
 };
 
 class Object3D
 {
 public:
   std::vector<Point> vertices;
-  std::vector<int[2]> edges;
+  // std::vector<int[2]> edges;
   Point center = Point(0, 0, 0);
   Orientation orient = Orientation(0, 0, 0);
 
-  Object3D(std::vector<Point> vertices, std::vector<int[2]> edges)
+  // Object3D(std::vector<Point> vertices, std::vector<int[2]> edges)
+  //   : vertices(vertices)
+  //   , edges(edges)
+  // {
+  // }
+
+  Object3D(std::vector<Point> vertices)
     : vertices(vertices)
-    , edges(edges)
   {
   }
 
   std::vector<Point> get_vertices()
   {
     std::vector<Point> new_vertices;
-    for (auto v : this->vertices) {
+    for (auto v : vertices) {
       new_vertices.push_back(
         v.rotate_x(orient.alpha).rotate_y(orient.beta).rotate_z(orient.gamma) +
         center);
     }
+
+    return new_vertices;
   }
 };
 
-// void
-// rotate_vertices(std::vector<Point>& vertices, double da, double db, double
-// dg)
-// {
-//   for (int i = 0; i < vertices.size(); i++) {
-//     rotate_x(vertices[i], da);
-//     rotate_y(vertices[i], db);
-//     rotate_z(vertices[i], dg);
-//   }
-// }
-
 int
-is_drawn(int i, int j, std::vector<Point>& vertices, double z)
+is_drawn(int i, int j, std::vector<Point> vertices, double z)
 {
   for (auto v : vertices) {
     if (std::round(v.x / (z)) == i && std::round(v.y / (z)) == j) {
       if (v.z <= -2)
-        return 3;
-      else if (v.z >= 2)
         return 1;
+      else if (v.z >= 2)
+        return 3;
       else
         return 2;
     }
@@ -126,44 +119,52 @@ int
 main()
 {
   using namespace std::chrono;
-  int fps = 90;
+  int fps = 120;
   int frame_time = 1000 / fps;
-  double speed = 1.5;
+  double speed = 0.5;
 
   std::vector<Point> vertices = {
     Point(4, 4, -4), Point(4, -4, -4), Point(-4, 4, -4), Point(-4, -4, -4),
     Point(4, 4, 4),  Point(4, -4, 4),  Point(-4, 4, 4),  Point(-4, -4, 4),
   };
 
+  Object3D cube(vertices);
+
   std::cout << "\033[2J" << std::endl;
-  double z = 0;
+  double theta = 0;
+
+  std::vector<Point> new_vertices = cube.get_vertices();
   while (true) {
-    int term_x = 64, term_y = 32;
+    int term_x = 48, term_y = 36;
 
     for (int i = 0; i < term_y; i++) {
       for (int j = 0; j < term_x; j++) {
-        int drawn = is_drawn(
-          j - term_x / 2, i - term_y / 2, vertices, 0.7 + 0.15 * std::sin(z));
+        int drawn = is_drawn(j - term_x / 2,
+                             i - term_y / 2,
+                             new_vertices,
+                             0.8 + 0.2 * std::sin(theta));
         switch (drawn) {
           case 3:
-            std::cout << "  ";
+            std::cout << "..";
             break;
           case 2:
-            std::cout << "--";
+            std::cout << "**";
             break;
           case 1:
-            std::cout << "++";
+            std::cout << "@@";
             break;
           case 0:
-            std::cout << "$$";
+            std::cout << "  ";
             break;
         }
       }
       std::cout << "\n";
     }
-    rotate_vertices(vertices, 0.001 * speed, 0.01 * speed, 0.01 * speed);
+    cube.orient =
+      cube.orient + Orientation(0.001 * speed, 0.05 * speed, 0.01 * speed);
     std::this_thread::sleep_for(frame_time * 1ms);
+    new_vertices = cube.get_vertices();
+    theta += 0.01;
     std::cout << "\033[3J" << std::endl;
-    z += 0.015;
   }
 }
